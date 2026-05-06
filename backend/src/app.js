0275e35524abfaf7d793a6964d5325a32fe6e157
nitesh404240 <niteshchoudharyjaat05@gmail.com>
2026-05-06 20:16:42 +0530

    change

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,10 @@ import express from "express"
 import cors from "cors";
 const app = express();
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
-import blog_routes from "./routes/blog_routes.js"
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));import blog_routes from "./routes/blog_routes.js"
 
 app.use("/blog/" , blog_routes);
 
